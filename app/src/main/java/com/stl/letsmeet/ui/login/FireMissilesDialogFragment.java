0e3f5dc5c8400bfe2c45d2b4c82852e41292cde0
Dune Zerna <duzer17@student.sdu.dk>
2020-10-28 23:14:04 +0100

    Initial commit Preferences added. - Dune Zerna
    # Conflicts:
    #	README.md

---
+++ app/src/main/java/com/stl/letsmeet/ui/login/FireMissilesDialogFragment.java
@@ -0,0 +1,32 @@
+package com.stl.letsmeet.ui.login;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.stl.letsmeet.R;
+
+
+public class FireMissilesDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Test Message")
+                .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
--- app/src/main/java/com/stl/letsmeet/ui/login/RegisterActivity.java
+++ app/src/main/java/com/stl/letsmeet/ui/login/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.stl.letsmeet.ui.login;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.stl.letsmeet.Profile;
@@ -17,25 +21,77 @@ import com.stl.letsmeet.R;
 public class RegisterActivity extends AppCompatActivity {
 
 
+    //private FireMissilesDialogFragment dialogFragment = DialogFragment;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
         final Button createButton = findViewById(R.id.registerLayout_register);
         final Button cancelButton = findViewById(R.id.registerLayout_cancel);
 
+
+        final EditText firstNameInput = findViewById(R.id.firstName);
+        final EditText lastNameInput = findViewById(R.id.lastName);
+        final EditText emailInput = findViewById(R.id.promptEmail);
+        final EditText postalCodeInput = findViewById(R.id.postalCode);
+        final EditText passwordInput = findViewById(R.id.promptPassword);
+        final EditText passwordRepeatInput = findViewById(R.id.promptPasswordAgain);
+        final Button registerButton = findViewById(R.id.register_button);
+
+        final String userEmail = null;
+
         Toast toast = Toast.makeText(getApplicationContext(), "RegisterActivity", Toast.LENGTH_LONG);
         toast.show();
+
 
         // Start Register activity
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
+
+                if (firstNameInput.getText().toString().isEmpty() || lastNameInput.getText().toString().isEmpty() || emailInput.getText().toString().isEmpty() || postalCodeInput.getText().toString().isEmpty() ||
+                        passwordInput.getText().toString().isEmpty() || passwordRepeatInput.getText().toString().isEmpty()) {
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "All fields were not filled", Toast.LENGTH_LONG);
+                    toast.show();
+
+                } else {
+
+                    // Storing data into SharedPreferences
+                    SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+
+                    // Creating an Editor object
+                    // to edit(write to the file)
+                    SharedPreferences.Editor saveProfile = sharedPreferences.edit();
+
+                    // Storing the key and its value
+                    // as the data fetched from edittext
+                    saveProfile.putString("firstName", firstNameInput.getText().toString());
+                    saveProfile.putString("lastName", lastNameInput.getText().toString());
+                    saveProfile.putString("email", emailInput.getText().toString());
+                    saveProfile.putString("postalCode", postalCodeInput.getText().toString());
+                    saveProfile.putString("password", passwordInput.getText().toString());
+
+
+                    // Once the changes have been made,
+                    // we need to commit to apply those changes made,
+                    // otherwise, it will throw an error
+                    saveProfile.apply();
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Your profile was saved successfully", Toast.LENGTH_LONG);
+                    toast.show();
+
+                    //dialogFragment.show(dialogFragment,"Tag");
+                }
+
+                //Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                //startActivity(intent);
+
             }
         });
+
+
 
         // Cancel button
         cancelButton.setOnClickListener(new View.OnClickListener() {
@@ -47,13 +103,6 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
-        final EditText firstNameInput = findViewById(R.id.firstName);
-        final EditText lastNameInput = findViewById(R.id.lastName);
-        final EditText emailInput = findViewById(R.id.promptEmail);
-        final EditText postalCodeInput = findViewById(R.id.postalCode);
-        final EditText passwordInput = findViewById(R.id.promptPassword);
-        final EditText passwordRepeatInput = findViewById(R.id.promptPasswordAgain);
-        final Button registerButton = findViewById(R.id.register_button);
 
     }
 }
